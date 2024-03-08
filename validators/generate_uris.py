1ce3dffa3475b47b5b8be0fa80c2be2324666348
namrata1012 <nsharma4@usc.edu>
2024-03-07 22:36:12 -0800

    Adding message for ttl validation

--- validators/generate_uris.py
+++ validators/generate_uris.py
@@ -1,6 +1,9 @@
 import re
 import json
 import uuid
+from slugify import slugify
+import hashlib
+from urllib.parse import urlparse
 
 def mineral_site_uri(site):
     try:
@@ -64,19 +67,21 @@ def process_mineral_site(ms):
 
     if 'source_id' in ms and 'record_id' in ms:
         merged_string = (f"{ms['source_id']}-{str(ms['record_id'])}")
-        merged_string = slugify(merged_string)
+        merged_string = custom_slugify(merged_string)
     elif 'source_id' in ms:
         merged_string = (f"{ms['source_id']}")
-        merged_string = slugify(merged_string)
+        merged_string = custom_slugify(merged_string)
     elif 'record_id' in ms:
         merged_string = (f"{ms['record_id']}")
-        merged_string = slugify(merged_string)
+        merged_string = custom_slugify(merged_string)
     else:
         return str(uuid.uuid4())
 
     if merged_string == '':
         return str(uuid.uuid4())
-    return merged_string
+
+    hashed_string = trim_and_append_hash(merged_string)
+    return hashed_string
 
 
 
@@ -90,73 +95,78 @@ def process_mineral_system(ms):
         if f in ms:
             f_object = ms[f]
             if 'theoretical' in f_object:
-                merged_string += slugify(f_object['theoretical'])
+                merged_string += custom_slugify(f_object['theoretical'])
             if 'criteria' in f_object:
-                merged_string += slugify(f_object['criteria'])
+                merged_string += custom_slugify(f_object['criteria'])
 
     if 'deposit_type' in ms:
         for dt in ms['deposit_type']:
-            merged_string += slugify(dt)
+            merged_string += custom_slugify(dt)
 
 
     if merged_string == '':
-        return ""
-    return merged_string
+        return str(uuid.uuid4())
+
+    hashed_string = trim_and_append_hash(merged_string)
+    return hashed_string
 
 
 def process_deposit_type(data):
     merged_string = ''
     if 'observed_name' in data:
-        merged_string = merged_string + slugify(data['observed_name'])
+        merged_string = merged_string + custom_slugify(data['observed_name'])
     merged_string += '-'
 
     if 'source' in data:
-        merged_string = merged_string + slugify(data['source'])
+        merged_string = merged_string + custom_slugify(data['source'])
     merged_string += '-'
 
     if 'normalized_uri' in data:
-        merged_string = merged_string+ slugify(data['normalized_uri'])
+        merged_string = merged_string+ custom_slugify(data['normalized_uri'])
     merged_string += '-'
 
     if 'confidence' in data:
-        merged_string = merged_string + slugify(str(data['confidence']))
+        merged_string = merged_string + custom_slugify(str(data['confidence']))
     merged_string += '-'
 
     if merged_string == '':
         return str(uuid.uuid4())
 
-    return merged_string
+    hashed_string = trim_and_append_hash(merged_string)
+    return hashed_string
 
 def process_document(data):
     merged_string = ''
     if 'doi' in data:
-        merged_string = merged_string + slugify(data['doi'])
+        merged_string = merged_string + custom_slugify(data['doi'])
     merged_string += '-'
 
     if 'uri' in data:
-        merged_string = merged_string + slugify(data['uri'])
+        merged_string = merged_string + custom_slugify(data['uri'])
     merged_string += '-'
 
     if 'title' in data:
-        merged_string = merged_string+ slugify(data['title'])
+        merged_string = merged_string+ custom_slugify(data['title'])
     merged_string += '-'
 
     if 'year' in data:
-        merged_string = merged_string + slugify(str(data['year']))
+        merged_string = merged_string + custom_slugify(str(data['year']))
     merged_string += '-'
 
     if 'authors' in data:
-        merged_string = merged_string  + slugify(str(data['authors']))
+        merged_string = merged_string  + custom_slugify(str(data['authors']))
     merged_string += '-'
 
     if 'month' in data:
-        merged_string = merged_string + slugify(str(data['month']))
+        merged_string = merged_string + custom_slugify(str(data['month']))
     merged_string += '-'
 
     if merged_string == '':
         return str(uuid.uuid4())
 
-    return merged_string
+    hashed_string = trim_and_append_hash(merged_string)
+
+    return hashed_string
 
 def process_mineral_inventory(ms, id):
     merged_string = ''
@@ -172,26 +182,48 @@ def process_mineral_inventory(ms, id):
         commodity = process_mi['commodity']
         category_str = ','.join(process_mi.get('category', []))
 
-        merged_string += (uri_ms + '-' + uri_doc + '-' + slugify(commodity) + '-' + slugify(category_str))
+        merged_string += (uri_ms + '-' + uri_doc + '-' + custom_slugify(commodity) + '-' + custom_slugify(category_str))
 
 
     if merged_string == '':
-        return ""
-    return merged_string
+        return str(uuid.uuid4())
+
+    hashed_string = trim_and_append_hash(merged_string)
+
+    return hashed_string
 
 
-def slugify(s):
+
+def trim_and_append_hash(string):
+    if len(string) > 50:
+        trimmed_string = string[:50]
+    else:
+        trimmed_string = string
+
+    string_hash = hashlib.sha256(string.encode()).hexdigest()
+
+    return trimmed_string + string_hash
+
+def remove_http(url):
+    parsed_url = urlparse(url)
+    prefix = 'https://minmod.isi.edu/resource'
+    if parsed_url.scheme + "://" + parsed_url.netloc == prefix:
+        return parsed_url.path
+    else:
+        return url
+
+    # if url.startswith("https://"):
+    #     return url[len("https://"):]
+    # elif url.startswith("http://"):
+    #     return url[len("http://"):]
+    # else:
+    #     return url
+
+def custom_slugify(s):
     ''' Simplifies ugly strings into something URL-friendly.
     slugify("[Some] _ Article's Title--"): some-articles-title. '''
 
     s = s.lower()
-    for c in [' ', '-', '.', '/']:
-        s = s.replace(c, '_')
-    s = re.sub('\W', '', s)
-    s = s.replace('_', ' ')
-    s = re.sub('\s+', ' ', s)
-    s = s.strip()
-    s = s.replace(' ', '')
+    s = remove_http(s) if s.startswith("http") else s
+    slug = slugify(s)
     return s
-
-
