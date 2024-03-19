3dc85ad52449c6f8d4b91e9b1d050e47bb83d4b3
namrata1012 <nsharma4@usc.edu>
2024-03-18 18:49:41 -0700

    Add test file

--- validators/validator_utils.py
+++ validators/validator_utils.py
@@ -33,7 +33,7 @@ def mineral_inventory_uri(param1):
 
 
 def remove_non_printable_chars(text):
-    clean_text = text.replace('\n', ' ').replace('\\u000b', '').replace('\\"', '$$$$').replace('$$$$', ' ')
+    clean_text = text.replace('\\u000b', '').replace('\\"', '$$$$').replace('$$$$', ' ')
     clean_text = clean_text.replace('\\n', '')
     return clean_text
 
