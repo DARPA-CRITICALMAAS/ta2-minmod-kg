88fab07af65cb3e6c8264a3c2bfed0b66ab8ce34
namrata1012 <nsharma4@usc.edu>
2024-04-24 12:31:19 -0700

    Make grade values optional

--- validators/create_ttl_files.py
+++ validators/create_ttl_files.py
@@ -3,7 +3,7 @@ import validate_pyshacl
 
 def run_drepr_on_file(datasource, model_file):
     destination = 'generated_files/ttl_files/'
-    command = f' python -m drepr -r {model_file} -d default="{datasource}"'
+    command = f' python -m drepr {model_file} default="{datasource}"'
     print('Running ... ', command)
 
     try:
@@ -18,12 +18,12 @@ def run_drepr_on_file(datasource, model_file):
 
 
 def run_drepr_on_mineral_site(datasource):
-    model_file = 'generator/model.yml'
+    model_file = 'generator/model_mineral_site.yml'
     return run_drepr_on_file(datasource, model_file)
 
 
 def run_drepr_on_mineral_system(datasource):
-    model_file = 'generator/model_mineral_system.yml'
+    model_file = 'generator/model_mineral_system_v2.yml'
     return run_drepr_on_file(datasource, model_file)
 
 
