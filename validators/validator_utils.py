9721f97fa9e7af1d8a7bbaf706c5f6fc9c89d2e8
namrata1012 <nsharma4@usc.edu>
2024-04-24 15:11:20 -0700

    Remove unused missing values

--- validators/validator_utils.py
+++ validators/validator_utils.py
@@ -175,7 +175,7 @@ def mineral_site_schema():
                                                 "anyOf": [
                                                     { "type": "number" },
                                                     { "type": "string" },
-                                                    { "type": "null" }
+                                                    { "type": None }
                                                 ]
                                             }
                                         }
@@ -188,7 +188,7 @@ def mineral_site_schema():
                                                 "anyOf": [
                                                     { "type": "number" },
                                                     { "type": "string" },
-                                                    { "type": "null" }
+                                                    { "type": None }
                                                 ]
                                             }
                                         }
@@ -201,7 +201,7 @@ def mineral_site_schema():
                                                 "anyOf": [
                                                     { "type": "number" },
                                                     { "type": "string" },
-                                                    { "type": "null" }
+                                                    { "type": None }
                                                 ]
                                             }
                                         }
