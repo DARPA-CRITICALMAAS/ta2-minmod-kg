53d1f7b8da1fd3d1f2499bcebcdda4460d7fcb63
namrata1012 <nsharma4@usc.edu>
2024-04-24 13:35:38 -0700

    Make grade values optional

--- validators/validate_pyshacl.py
+++ validators/validate_pyshacl.py
@@ -192,7 +192,7 @@ def validate_using_shacl_mineral_site(data_graph):
         sh:property [   
             sh:path mndr:contained_metal ;
             sh:minCount 0 ;
-            sh:or ( [ sh:datatype xsd:decimal ] [ sh:datatype xsd:integer ] ) ;
+            sh:or ( [ sh:datatype xsd:decimal ] [ sh:datatype xsd:integer ] [ sh:datatype xsd:double ] ) ;
     
         ];
         .
