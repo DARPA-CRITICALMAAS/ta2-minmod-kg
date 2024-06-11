84a65493f39c72023ae30027253dbe9637b67edb
Binh Vu <binh@toan2.com>
2024-06-10 22:03:54 -0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-set -x
-
 CWD=$(pwd)
+UID=$(id -u)
+GID=$(id -g)
+
+set -x
 
 # start ETL service
 docker run --name etl --rm --network host \
     -w $CWD \
     -v $CWD:$CWD \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    -u $UID:$GID \
     minmod-etl python -m statickg ./ta2-minmod-kg/etl.yml ./kgdata ./ta2-minmod-data
