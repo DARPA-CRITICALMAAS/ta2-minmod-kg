a85926d9b474e5b22f6a8a5c84245f06284cc21a
Binh Vu <binh@toan2.com>
2024-08-23 09:46:29 -0400

    fix Neodymium typo

--- minmodkg/integrations/cdr.py
+++ minmodkg/integrations/cdr.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
         "Cerium",
         "Yttrium",
         "Praseodymium",
-        "Neoymium",
+        "Neodymium",
         "Samarium",
         "Europium",
         "Terbium",
