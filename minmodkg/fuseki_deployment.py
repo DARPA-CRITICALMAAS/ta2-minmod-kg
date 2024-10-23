a4036d1eebc92e3d2799c74aec2a4587c3c9eb26
Binh Vu <binh@toan2.com>
2024-10-23 12:39:41 -0700

    update fuseki deployment

--- minmodkg/fuseki_deployment.py
+++ minmodkg/fuseki_deployment.py
@@ -50,7 +50,7 @@ class FusekiDeploymentService(BaseService[FusekiDeploymentServiceInvokeArgs]):
         self.args = args
         self.hostname = args.get("hostname", "http://localhost")
         self.current_dbinfo = None
-        self.starting_port = int(os.environ.get("FUSEKI_PORT", "3030"))
+        self.fuseki_port = int(os.environ.get("FUSEKI_PORT", "3030"))
 
     def forward(
         self,
@@ -63,17 +63,17 @@ class FusekiDeploymentService(BaseService[FusekiDeploymentServiceInvokeArgs]):
         (dbinfo,) = lst
 
         if not dbinfo.has_running_service():
-            if not is_port_available(self.hostname, self.starting_port):
+            if not is_port_available(self.hostname, self.fuseki_port):
                 subprocess.check_call(self.get_stop_all_command(args), shell=True)
 
             if not wait_till_port_available(
-                self.hostname, self.starting_port, timeout=10
+                self.hostname, self.fuseki_port, timeout=10
             ):
                 self.logger.error(
-                    f"After stopping all fuseki services, port {self.starting_port} is still not available."
+                    f"After stopping all fuseki services, port {self.fuseki_port} is still not available."
                 )
                 raise Exception(
-                    f"Another one started a service on port {self.starting_port} that is not managed by this service"
+                    f"Another one started a service on port {self.fuseki_port} that is not managed by this service"
                 )
 
             # only deploy the service when there is no running service for the directory
@@ -92,7 +92,7 @@ class FusekiDeploymentService(BaseService[FusekiDeploymentServiceInvokeArgs]):
             return False
 
         name = f"fuseki-{dbinfo.dir.name}"
-        port = find_available_port(self.hostname, self.starting_port)
+        port = find_available_port(self.hostname, self.fuseki_port)
         try:
             subprocess.check_call(
                 self.get_start_command(args).format(
