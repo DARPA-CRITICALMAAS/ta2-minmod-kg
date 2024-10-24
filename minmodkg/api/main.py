f3213877090a8e1ed55df23dcd5667d54ec6d621
Binh Vu <binh@toan2.com>
2024-10-23 21:12:23 -0700

    customize API endpoint

--- minmodkg/api/main.py
+++ minmodkg/api/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -23,16 +24,20 @@ async def lifespan(app: FastAPI):
     yield
 
 
+API_ENDPOINT = os.environ.get("MINMOD_API_ENDPOINT", "/api/v1")
+
 app = FastAPI(
-    openapi_url="/api/v1/openapi.json", docs_url="/api/v1/docs", lifespan=lifespan
+    openapi_url="/api/v1/openapi.json",
+    docs_url=f"{API_ENDPOINT}/docs",
+    lifespan=lifespan,
 )
 
-app.include_router(commodity.router, prefix="/api/v1")
-app.include_router(dedup_mineral_site.router, prefix="/api/v1")
-app.include_router(deposit_type.router, prefix="/api/v1")
+app.include_router(commodity.router, prefix=API_ENDPOINT)
+app.include_router(dedup_mineral_site.router, prefix=API_ENDPOINT)
+app.include_router(deposit_type.router, prefix=API_ENDPOINT)
 app.include_router(lod.router)
-app.include_router(login.router, prefix="/api/v1")
-app.include_router(mineral_site.router, prefix="/api/v1")
-app.include_router(stats.router, prefix="/api/v1")
-app.include_router(unit.router, prefix="/api/v1")
-app.include_router(admin.router, prefix="/api/v1/admin")
+app.include_router(login.router, prefix=API_ENDPOINT)
+app.include_router(mineral_site.router, prefix=API_ENDPOINT)
+app.include_router(stats.router, prefix=API_ENDPOINT)
+app.include_router(unit.router, prefix=API_ENDPOINT)
+app.include_router(admin.router, prefix=f"{API_ENDPOINT}/admin")
