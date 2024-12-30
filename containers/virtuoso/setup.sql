0956faf1f9e95bbc05701190109d436cac5674aa
Binh Vu <bvu687@gmail.com>
2024-12-30 14:56:24 -0800

    update gitignore

---
+++ containers/virtuoso/setup.sql
@@ -0,0 +1,3 @@
+grant execute on DB.DBA.SPARQL_MODIFY_BY_DICT_CONTENTS to "SPARQL" ;
+grant SPARQL_UPDATE to "SPARQL" ;
+DB.DBA.RDF_GRAPH_USER_PERMS_SET ('https://purl.org/drepr/1.0/', 'SPARQL', 3);
