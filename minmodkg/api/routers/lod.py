ed43bdf543b03e01d9f0599c24e5d0ca2c20b1ac
Binh Vu <bvu687@gmail.com>
2025-01-09 15:06:01 -0800

    derived mineral site in lod should now open derived site

--- minmodkg/api/routers/lod.py
+++ minmodkg/api/routers/lod.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from typing import Annotated, Literal, Optional
+from typing import Annotated, Literal, Mapping, Optional, Sequence
 from urllib.parse import urlparse, urlunparse
 
 import htbuilder as H
@@ -9,7 +9,10 @@ import rdflib
 import rdflib.term
 from fastapi import APIRouter, HTTPException, Query
 from fastapi.responses import HTMLResponse
+from minmodkg.api.models.public_dedup_mineral_site import DedupMineralSitePublic
 from minmodkg.models.base import MINMOD_KG, MINMOD_NS
+from minmodkg.services.mineral_site_v2 import MineralSiteService
+from minmodkg.typing import IRI
 from rdflib import OWL, RDF, RDFS, BNode, Graph
 from rdflib import Literal as RDFLiteral
 from rdflib import URIRef
@@ -77,14 +80,34 @@ def get_derived(
     format: Annotated[Literal["html", "json"], Query()] = "html",
     remove_hostname: Annotated[Literal["yes", "no"], Query()] = "no",
 ):  # -> HTMLResponse | dict[Any, Any]:
+    if resource_id.startswith("dedup_site__"):
+        same_as_sites = MineralSiteService().find_dedup_mineral_sites(
+            commodity=None, dedup_site_ids=[resource_id]
+        )
+        if len(same_as_sites) == 0:
+            raise HTTPException(status_code=404, detail="Resource not found")
+        dedup_site = DedupMineralSitePublic.from_kgrel(
+            same_as_sites[resource_id], commodity=None
+        )
+        if format == "json":
+            return dedup_site.model_dump(exclude_none=True)
+        if format == "html":
+            return render_dict_html(
+                dedup_site.name,
+                MINMOD_NS.md.uri(resource_id),
+                dedup_site.model_dump(exclude_none=True),
+            )
+
     uri = MINMOD_NS.md.uri(resource_id)
     if not MINMOD_KG.has(uri):
         raise HTTPException(status_code=404, detail="Resource not found")
 
     if format == "html":
         return render_entity_html(
-            uri, remove_hostname=get_hostname() if remove_hostname == "yes" else None
+            uri,
+            remove_hostname=get_hostname() if remove_hostname == "yes" else None,
         )
+
     if format == "json":
         return render_entity_json(uri)
     raise HTTPException(status_code=400, detail="Invalid format")
@@ -113,8 +136,9 @@ def get_entity_data(subj: URIRef) -> Graph:
     )
 
 
-def render_entity_json(subj: URIRef):
-    g = get_entity_data(subj)
+def render_entity_json(subj: URIRef, g: Optional[Graph] = None):
+    if g is None:
+        g = get_entity_data(subj)
 
     def label(obj: rdflib.term.Node):
         if obj == RDFS.label:
@@ -164,8 +188,11 @@ def render_entity_json(subj: URIRef):
     return out
 
 
-def render_entity_html(subj: URIRef, remove_hostname: Optional[str] = None):
-    g = get_entity_data(subj)
+def render_entity_html(
+    subj: URIRef, remove_hostname: Optional[str] = None, g: Optional[Graph] = None
+):
+    if g is None:
+        g = get_entity_data(subj)
 
     def label(g, subj: rdflib.term.Node):
         if (subj, RDFS.label, None) in g:
@@ -259,6 +286,71 @@ def render_entity_html(subj: URIRef, remove_hostname: Optional[str] = None):
 <html>
     <head>
         <title>{subj_label}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <style>a {{ text-decoration: none; }}</style>
+    </head>
+    <body>
+        {tree}
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    </body>
+</html>
+                        """,
+        status_code=200,
+    )
+
+
+def render_dict_html(
+    obj_name: str, obj_uri: IRI, obj: dict, remove_hostname: Optional[str] = None
+):
+    def get_href(subj: IRI):
+        if remove_hostname is not None and subj.startswith(remove_hostname):
+            return subj[len(remove_hostname) :] + "?remove_hostname=yes"
+        return subj
+
+    def make_tree(d: dict | list | str | bool | int | float):
+        if not isinstance(d, (dict, list)):
+            return H.p(d)
+        if isinstance(d, list):
+            return H.table(_class="table")(*[H.tr(make_tree(x)) for x in d])
+        children = []
+        for k, v in d.items():
+            children.append(
+                H.tr(
+                    H.td(H.a()(k)),
+                    H.td(make_tree(v)),
+                )
+            )
+        return H.table(_class="table")(*children)
+
+    children = []
+    for k, v in obj.items():
+        children.append((H.a()(k), make_tree(v)))
+
+    tree = H.div(_class="container-fluid")(
+        H.div(_class="row", style="margin-top: 20px; margin-bottom: 20px")(
+            H.div(_class="col")(
+                H.h4(
+                    H.a(href=get_href(obj_uri))(obj_name),
+                ),
+                H.small(_class="text-muted fw-semibold")(obj_uri),
+            )
+        ),
+        H.table(_class="table table-striped")(
+            *[
+                H.tr(
+                    H.td(p),
+                    H.td(o),
+                )
+                for p, o in children
+            ]
+        ),
+    )
+    return HTMLResponse(
+        content=f"""
+<html>
+    <head>
+        <title>{obj_name}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <style>a {{ text-decoration: none; }}</style>
     </head>
